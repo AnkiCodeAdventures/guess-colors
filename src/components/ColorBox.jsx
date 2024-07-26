755fe01223b3e3b740af9fe781ec2b46b16d8bdd
Ankita <ankitaag94@gmail.com>
2024-07-27 01:41:16 +0530

    style: improve layout

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -37,58 +37,62 @@ function ColorBox() {
         height: "100vh",
         flexDirection: "column",
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "flex-start",
+        gap: "2rem",
+        paddingTop: "2rem",
       }}
     >
       <Typography variant="h1" color="white">
         Guess The Color !
       </Typography>
-      <div>
-        <Box
-          maxWidth="sm"
-          style={{
-            width: "200px",
-            height: "200px",
-            backgroundColor: `${correctColor}`,
-          }}
-        />
 
-        <div style={{ margin: "3rem 0" }}>
-          {colors.map((color) => {
-            return (
-              <Button
-                variant="text"
-                onClick={(event) => {
-                  handleClick(event.target.innerText);
-                }}
-                key={color.hexColor}
-              >
-                <Typography variant="h5" color="white" gutterBottom>
-                  {color.hexColor}
-                </Typography>
-              </Button>
-            );
-          })}
-          {isCorrectAnswer !== null ? (
-            isCorrectAnswer ? (
-              <Alert
-                icon={<CheckIcon fontSize="large" />}
-                severity="success"
-                variant="filled"
-              >
-                <Typography variant="h5">The answer is correct !</Typography>
-              </Alert>
-            ) : (
-              <Alert
-                icon={<FmdBadIcon fontSize="large" />}
-                severity="error"
-                variant="filled"
-              >
-                <Typography variant="h5">Wrong Answer, Try Again !</Typography>
-              </Alert>
-            )
-          ) : null}
-        </div>
+      <Box
+        maxWidth="sm"
+        style={{
+          width: "200px",
+          height: "200px",
+          backgroundColor: `${correctColor}`,
+        }}
+      />
+      <div>
+        {colors.map((color) => {
+          return (
+            <Button
+              variant="outlined"
+              style={{ margin: "0.8rem" }}
+              onClick={(event) => {
+                handleClick(event.target.innerText);
+              }}
+              key={color.hexColor}
+            >
+              <Typography variant="h5" color="white" gutterBottom>
+                {color.hexColor}
+              </Typography>
+            </Button>
+          );
+        })}
+      </div>
+      <div style={{ minHeight: "4rem" }}>
+        {isCorrectAnswer !== null ? (
+          isCorrectAnswer ? (
+            <Alert
+              icon={<CheckIcon fontSize="large" />}
+              severity="success"
+              variant="filled"
+              style={{ margin: "0 1.3rem" }}
+            >
+              <Typography variant="h5">The answer is correct !</Typography>
+            </Alert>
+          ) : (
+            <Alert
+              icon={<FmdBadIcon fontSize="large" />}
+              severity="error"
+              variant="filled"
+            >
+              <Typography variant="h5">Wrong Answer, Try Again !</Typography>
+            </Alert>
+          )
+        ) : null}
       </div>
     </div>
   );
